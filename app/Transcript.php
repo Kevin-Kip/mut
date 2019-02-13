eae3f39dd5736e4476f7ea17f0f3e099114ea5ca
Kevin-Kip <masterfork5@gmail.com>
2019-02-13 14:36:44 +0300

    More changes

--- app/Transcript.php
+++ app/Transcript.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transcript extends Model
 {
-    //
+    protected $fillable = [
+        'transcript_id',
+        'student',
+        'semester'
+    ];
 }
